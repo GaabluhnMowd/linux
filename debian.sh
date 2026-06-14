c6f0f4b053c17200debdc947cc59b1681826cf21
GaabluhnMowd <145775082+GaabluhnMowd@users.noreply.github.com>
2026-06-14 11:35:54 -0400

    Update debian.sh

--- debian.sh
+++ debian.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
 sudo apt update
+sudo apt install --no-install-recommends kde-plasma-desktop
+sudo apt install plasma-pa konsole mousepad
+sudo apt install plasma-nm
+sudo apt install powerdevil
+sudo apt install polkit-kde-agent-1
+sudo apt install xdg-desktop-portal-kde
+sudo apt install kde-gtk-config
+sudo apt install network-manager
+sudo systemctl enable NetworkManager
+sudo systemctl start NetworkManager
